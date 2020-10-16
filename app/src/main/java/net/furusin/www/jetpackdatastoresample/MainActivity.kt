7c7190a91e42c6ab991821b00fee94e2eebca9e2
furusin <furusin.oriver@gmail.com>
2020-10-17 00:51:16 +0900

    add using Preferences DataStore

--- app/src/main/java/net/furusin/www/jetpackdatastoresample/MainActivity.kt
+++ app/src/main/java/net/furusin/www/jetpackdatastoresample/MainActivity.kt
@@ -1,11 +1,67 @@
 package net.furusin.www.jetpackdatastoresample
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.DataStore
+import androidx.datastore.createDataStore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dataStore: DataStore<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dataStore = createDataStore(fileName = "preferences", serializer = UserSerializer)
+
+        findViewById<Button>(R.id.saveAgeButton).setOnClickListener {
+            GlobalScope.launch {
+                setUserAge(20)
+            }
+        }
+
+        findViewById<Button>(R.id.saveNameButton).setOnClickListener {
+            GlobalScope.launch {
+                setUserName("furusin")
+            }
+        }
+
+        findViewById<Button>(R.id.loadButton).setOnClickListener {
+            GlobalScope.launch {
+                getStoredUser().collect { Log.d("test", "age = ${it.age}, name = ${it.name}") }
+            }
+        }
     }
+
+    private suspend fun setUserAge(age: Int) {
+        dataStore.updateData { currentUser: User ->
+            currentUser.toBuilder()
+                .setAge(age)
+                .build()
+        }
+    }
+
+    private suspend fun setUserName(name: String) {
+        dataStore.updateData { currentUser: User ->
+            currentUser.toBuilder()
+                .setName(name)
+                .build()
+        }
+    }
+
+    private suspend fun storeUser(user: User) {
+        dataStore.updateData { currentUser ->
+            currentUser.toBuilder()
+                .setAge(user.age)
+                .setName(user.name)
+                .build()
+        }
+    }
+
+    private fun getStoredUser(): Flow<User> = dataStore.data
 }
---
+++ app/src/main/java/net/furusin/www/jetpackdatastoresample/UserSerializer.kt
@@ -0,0 +1,21 @@
+package net.furusin.www.jetpackdatastoresample
+
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object UserSerializer : Serializer<User> {
+    override fun readFrom(input: InputStream): User {
+        try {
+            return User.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override fun writeTo(t: User, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
